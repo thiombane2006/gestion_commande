dee6d0af3190d7d428ec710a86d1a27dae6e1af2
thiombane2006 <thiombanebayemarame@gmail.com>
2025-11-24 13:33:22 +0000

    creation entities utilisateur et Profil

--- src/main/java/sn/edu/isepat/tic/dfe/p6/entities/Utilisateur.java
+++ src/main/java/sn/edu/isepat/tic/dfe/p6/entities/Utilisateur.java
@@ -1,17 +1,19 @@
 package sn.edu.isepat.tic.dfe.p6.entities;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import org.eclipse.persistence.tools.profiler.Profile;
 
+@Entity
+@Table(name = "utilisateur")
 public class Utilisateur {
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY) // permet d'auto-incrementer
     private Long id;
 
     @Column(nullable = false)
     private String nom;
 
-    @Column(nullable = false)
+    @Column(nullable = false, unique = true) // unique = true veut dire que l'email sera unique
     private String email;
 
     private Profile profile;
