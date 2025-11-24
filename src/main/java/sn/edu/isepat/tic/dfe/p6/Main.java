1648c7d2089df30db277af951571152d46588cf1
thiombane2006 <thiombanebayemarame@gmail.com>
2025-11-24 12:58:36 +0000

    Test parametre de configuration du persistence.xml dans le main

--- src/main/java/sn/edu/isepat/tic/dfe/p6/Main.java
+++ src/main/java/sn/edu/isepat/tic/dfe/p6/Main.java
@@ -1,9 +1,19 @@
 package sn.edu.isepat.tic.dfe.p6;
 
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.Persistence;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("gestionProduitsPU");
+        EntityManager em = emf.createEntityManager();
 
+
+
+        em.close();
+        emf.close();
     }
 }
